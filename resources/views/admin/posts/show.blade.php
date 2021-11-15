3d4e7e52b64a30659dcefefe396110642c66660b
Jack-rm <jackcnsr94@gmail.com>
2021-11-15 16:47:05 +0100

    Html hotfix in show.blade

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -6,10 +6,17 @@
             <h1 class="card-title"> {{$post->title}} </h1>
             <address class="card-subtitle"> from {{ $post->author }} </address>
             <address class="card-subtitle date"> on {{ $post->getFormattedDate('post_date')}} </address>
+            <div class="card-body row">
+                <div class="col-4">
+                    <img class="img-fluid" src="{{$post->image_url}}" alt="{{$post->title}} image">
+                </div>
+                <div class="col-8">
+                    <p class="card-body"> {{$post->post_content}} </p>
+                </div>
+            </div>
 
-            <p class="card-body"> {{$post->post_content}} </p>
-            <div class="card-footer back-to-list">
-                <a href="{{route('admin.posts.index')}}" class="btn btn-toolbar">Torna alla lista dei post</a>
+            <div class="card-footer back-to-list d-flex justify-content-end">
+                <a href="{{route('admin.posts.index')}}" class="btn btn-toolbar"><u>Go back to posts</u></a>
             </div>
             
         </div>
