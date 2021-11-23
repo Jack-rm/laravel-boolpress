b48c94a5267f8b55a592c89bd77498e7e7522c99
Jack-rm <jackcnsr94@gmail.com>
2021-11-23 17:01:42 +0100

    Creating Roles table + seeding

---
+++ database/seeds/RolesTableSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Role;
+
+use Faker\Generator as Faker;
+
+class RolesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        $roleList = ['Administrator', 'Editor', 'Author', 'Contributor', 'Subscriber'];
+
+        for ($i = 1 ; $i <= 5; $i++){
+            $newRole = new Role();
+            $newRole->rank = $i + 1;
+            $newRole->name = $roleList[$i - 1];
+            $newRole->color = $faker->safeHexColor();
+            $newRole->save();
+        }
+
+        $role = new Role();
+        $role->rank = 1;
+        $role->name = 'Super Admin';
+        $role->color = $faker->safeHexColor();
+        $role->save();
+
+    }
+}
